c788834315ed78d5f29e515929f089a138222156
agasthyarkumar-divum <agasthya.kumar@divum.in>
2026-05-07 11:03:21 +0530

    Chore: Basic Skeleton

---
+++ admin-dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-slate-800 p-10 rounded-2xl shadow-xl">
+        <h1 className="text-4xl font-bold mb-4">
+          Chakravyuh Auth
+        </h1>
+
+        <p className="text-slate-300">
+          SuperAdmin Dashboard
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
